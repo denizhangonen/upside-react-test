20f5819668820805ad088fe1cc92fd533186783a
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-01-21 22:10:21 +0300

    Router has been implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div className="App">Dhan</div>;
-  }
+function App() {
+  return (
+    <div className="App">
+      Dhan 
+    </div>
+  );
 }
 
 export default App;
